921a54e9cd7c429813ec94a20552f917dcb267cf
Tomas Gavenciak <gavento@gmail.com>
2025-09-19 16:34:29 +0000

    Supabase config

--- frontend/src/config/supabase.ts
+++ frontend/src/config/supabase.ts
@@ -1,6 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL ||
+  "https://qtsteugcmlmdabngjrxz.supabase.co";
+const supabaseAnonKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY ||
+  "sb_publishable_0NDcI4rOH8L4P-cfPLT3Hw__KQn3KAQ";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

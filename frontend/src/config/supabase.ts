8f5efbd3336ebdc207eb481eeb9de09e61dfdb88
Tomas Gavenciak <gavento@gmail.com>
2025-09-19 21:57:12 +0000

    Update frontend config vars

--- frontend/src/config/supabase.ts
+++ frontend/src/config/supabase.ts
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL ||
+  import.meta.env.SUPABASE_URL ||
   "https://qtsteugcmlmdabngjrxz.supabase.co";
-const supabaseAnonKey =
-  import.meta.env.VITE_SUPABASE_ANON_KEY ||
+const supabasePublishableKey =
+  import.meta.env.SUPABASE_PUBLISHABLE_KEY ||
   "sb_publishable_0NDcI4rOH8L4P-cfPLT3Hw__KQn3KAQ";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabasePublishableKey);
